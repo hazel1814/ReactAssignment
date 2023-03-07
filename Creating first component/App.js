4cdea95302e9b54375c1d02be89edfad1143f637
hazel1814 <57265968+hazel1814@users.noreply.github.com>
2023-03-07 17:53:50 +0530

    Add files via upload

---
+++ Creating first component/App.js
@@ -0,0 +1,14 @@
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/Expenses/NewExpense/NewExpense";
+
+function App() {
+
+  return (
+    <div>
+      <NewExpense />
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
